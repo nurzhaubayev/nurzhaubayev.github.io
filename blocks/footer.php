666835d8f86ecf623e5561f37988097d11f3bd34
nurzhaubayev <49671987+nurzhaubayev@users.noreply.github.com>
2020-10-21 17:16:00 +0600

    First

---
+++ blocks/footer.php
@@ -0,0 +1,41 @@
+<section id="contacts">
+<footer class="container py-5 d-flex flex-column fixed-bottom">
+  <div class="pricing-header px-3 py-3 pb-md-4 mx-auto text-center">
+<h1 class="display-4">Контакты</h1>
+</div>
+<div class="row">
+<div class="d-flex flex-column " style="
+margin-right: 50px;
+">
+ <img src="img/L2.jpg" class="rounded" style="width: 200px;"  alt="...">
+    <p></p>
+
+</div>
+<div class="col-6 col-md">
+  <h5>Адрес:</h5>
+    <ul class="list-unstyled text-small">
+    <li><a class="text-muted" href="#">Республика Казахстан, 010000, город Астана,
+Бизнес центр «Ансар», ул. Сыганак 25, 9 этаж</a></li>
+  </ul>
+
+</div>
+<div class="col-6 col-md">
+  <h5>Телефон:</h5>
+  <ul class="list-unstyled text-small">
+    <li><a class="text-muted" href="#">+7 (7172) 244 711</a></li>
+    <li><a class="text-muted" href="#">+7 (7172) 911-911</a></li>
+    <li><a class="text-muted" href="#">+7 (702) 227 70 22</a></li>
+  </ul>
+</div>
+   <div class="col-6 col-md">
+  <h5>e-mail:</h5>
+  <ul class="list-unstyled text-small">
+    <li><a class="text-muted" href="#">info@kai.kz</a></li>
+  </ul>
+        <small class="d-block mb-3 text-muted">© 2007-2020</small>
+</div>
+
+</div>
+</footer>
+
+</section>
---
+++ blocks/header.php
@@ -0,0 +1,62 @@
+<header>
+<div class="d-flex flex-column flex-md-row align-items-center  px-md-4 mb-3 bg-white border-bottom shadow-sm">
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light">
+<a class="navbar-brand" href="index.php#">
+<img src="img/L2.jpg" class="rounded" style="width: 250px;"  alt="...">
+</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav ml-auto">
+  
+ <li class="nav-item" style="
+margin-top: 15px;
+">
+    <a class="p-2 text-dark" href=index.php#about>О нас</a>
+  </li>
+  <li class="nav-item" style="
+margin-top: 15px;
+">
+    <a class="p-2 text-dark" href=index.php#courses>Курсы</a>
+  </li>
+  <li class="nav-item" style="
+margin-top: 15px;
+">
+    <a class="p-2 text-dark" href="index.php#news">Новости</a>
+  </li>
+  <li class="nav-item" style="
+margin-top: 15px;
+">
+   <a class="p-2 text-dark" href="index.php#ref">Отзывы</a>
+  </li>
+  <li class="nav-item" style="
+margin-top: 15px;
+">
+    <a class="p-2 text-dark" href=index.php#contacts>Контакты</a>
+  </li>
+      <li class="nav-item" >
+         <a href="https://www.facebook.com/shakhmaran.seilov/" class="fa fa-facebook"></a>
+         </li>
+     <li class="nav-item">
+<a href="https://www.instagram.com/kai_astana/" class="fa fa-instagram"></a>
+         </li>
+          <li class="nav-item">
+    <a href="https://www.youtube.com/channel/UC1r-DgFZSgQf5F15R4pTbdA" class="fa fa-youtube"></a>
+</li>
+    <li class="nav-item" style="
+margin-top: 8px;
+">
+           <a class=" btn btn-outline-primary" href="application.php">Оставить заявку</a>
+    </li>
+</ul>
+
+</div>
+
+</nav>
+
+</div>
+
+</div>
+</header>
